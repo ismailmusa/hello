c0b438683c071d399e4d4d47dc0e9190c53a991e
ismail musa saidu <39607205+ismailmusa@users.noreply.github.com>
2023-02-19 15:18:36 +0100

    Update createTodo.ts

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,29 +1,38 @@
-import 'source-map-support/register'
-
-import {APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult} from 'aws-lambda'
+import 'source-map-support/register';
+import * as middy from 'middy';
+import { cors } from 'middy/middlewares';
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { createTodo } from '../../businessLogic/todos';
 import {CreateTodoRequest} from '../../requests/CreateTodoRequest';
-import {createTodo} from "../../businessLogic/Todo";
-import { createTodo } from '../../logicLayer/todo'
-const logger = createLogger('Todo')
+import {getUserId} from '../utils';
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    // TODO: Implement creating a new TODO item
-    
-    const authorization = event.headers.Authorization;
-    const split = authorization.split(' ');
-    const Token = split[1];
-
-    const Todo: CreateTodoRequest = JSON.parse(event.body);
-    logger.info("processing event ")
-    const Items = await createTodo(Todo, Token);
-
+export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const newTodo: CreateTodoRequest = JSON.parse(event.body);
+  const userId = getUserId(event);
+  console.log('Processing event: ', event);
+  try {
+    const todoAdded = await createTodo(newTodo, userId);
     return {
-        statusCode: 201,
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-        },
-        body: JSON.stringify({
-            "item": Items
-        }),
+      statusCode: 201,
+      body: JSON.stringify({
+        item: todoAdded
+      })
     }
-};
+
+  } catch (e) {
+    console.log(e)
+    return {
+      statusCode: e.status || 500,
+      body: JSON.stringify({
+        error: `Failed to create todo due to: ${e}`
+      })
+    }
+  }
+});
+
+handler.use(
+  cors({
+    credentials: true
+  })
+);
+
