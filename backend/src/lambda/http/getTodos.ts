44408009b5a64d0a68ed878320850b7fb45206c8
ismail musa saidu <39607205+ismailmusa@users.noreply.github.com>
2023-02-19 14:10:12 +0100

    Update getTodos.ts

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,16 +1,18 @@
 import 'source-map-support/register'
 
 import {APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler} from 'aws-lambda';
-import {getAllToDo} from "../../businessLogic/Todo";
+import {AllTodo} from "../../businessLogic/Todo";
+import { createLogger } from '../../utils/logger'
+const logger = createLogger('Todo')
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     // TODO: Get all TODO items for a current user
-    console.log("Processing Event ", event);
+    logger.info("processing event get todos", event)
     const authorization = event.headers.Authorization;
     const split = authorization.split(' ');
     const jwtToken = split[1];
 
-    const toDos = await getAllToDo(jwtToken);
+    const todos = await AllTodo(jwtToken);
 
     return {
         statusCode: 200,
@@ -18,7 +20,7 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
             "Access-Control-Allow-Origin": "*",
         },
         body: JSON.stringify({
-            "items": toDos,
+            "items": todos,
         }),
     }
 };
