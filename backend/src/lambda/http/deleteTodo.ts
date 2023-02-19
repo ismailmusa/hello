096842fa58ae9f8467ab3138743a4b5c0a6b3dac
ismail musa saidu <39607205+ismailmusa@users.noreply.github.com>
2023-02-19 14:25:20 +0100

    Update deleteTodo.ts

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -2,23 +2,24 @@ import 'source-map-support/register'
 
 import {APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler} from 'aws-lambda';
 import {deleteTodo} from "../../businessLogic/Todo";
+import { createLogger } from '../../utils/logger'
+const logger = createLogger('Todo')
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     // TODO: Remove a TODO item by id
-    console.log("Processing Event ", event);
     const authorization = event.headers.Authorization;
     const split = authorization.split(' ');
-    const jwtToken = split[1];
-
-    const todoId = event.pathParameters.todoId;
-
-    const deleteData = await deleteTodo(todoId, jwtToken);
+    const Token = split[1];
+    
+    const Id = event.pathParameters.todoId;
+    logger.info("Proccessing delete event on todo", event )
+    const Data = await deleteTodo(Id, Token);
 
     return {
         statusCode: 200,
         headers: {
             "Access-Control-Allow-Origin": "*",
         },
-        body: deleteData,
+        body: Data,
     }
 };
