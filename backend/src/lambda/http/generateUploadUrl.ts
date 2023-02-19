a39323f6b31899cc5f00d9a705c3614ee48ad380
ismail musa saidu <39607205+ismailmusa@users.noreply.github.com>
2023-02-19 14:28:14 +0100

    Update generateUploadUrl.ts

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -2,13 +2,13 @@ import 'source-map-support/register'
 
 import {APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler} from 'aws-lambda'
 import {UploadUrl} from "../../businessLogic/Todo";
-
+import { createLogger } from '../../utils/logger'
+const logger = createLogger('Todo')
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-    console.log("Processing Event ", event);
-    const todoId = event.pathParameters.todoId;
-
-    const URL = await UploadUrl(todoId);
+    const Id = event.pathParameters.todoId;
+    logger.info("Proccessing delete event on todo")
+    const URL = await UploadUrl(Id);
 
     return {
         statusCode: 202,
